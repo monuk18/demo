de53ca15dfab1b74e221274d22679f6cd45682bf
Divyanshu kumar <divyanshu.kumar@syncron.com>
2019-01-12 19:56:34 +0530

    Fix for falling tests

--- src/test/java/com/employee/demo/EmployeeRestControllerIntegrationTest.java
+++ src/test/java/com/employee/demo/EmployeeRestControllerIntegrationTest.java
@@ -1,52 +1,43 @@
 package com.employee.demo;
 
-import com.employee.demo.controller.EmployeeController;
-import com.employee.demo.entity.Employee;
-import com.employee.demo.service.EmployeeService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(EmployeeController.class)
+@ContextConfiguration(classes = DemoApplication.class)
+@SpringBootTest
 public class EmployeeRestControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mvc;
 
-    @MockBean
-    private EmployeeService service;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     // write test cases here
     @Test
-    public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
+    public void verifyAllEmployeeList()
             throws Exception {
-
-        Employee alex = new Employee("alex");
-
-        List<Employee> allEmployees = Arrays.asList(alex);
-
-        given(service.getAllEmployees()).willReturn(allEmployees);
-
-        mvc.perform(get("/employee/all")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)));
-                //.andExpect(jsonPath("$[0].city", is("alex")));
+        mockMvc.perform(MockMvcRequestBuilders.get("/employee/all").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("", hasSize(4))).andDo(print());
     }
 }
